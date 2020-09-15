49aa740f003e3e0ddc32f0afcd2dbc5341dee9e0
Osti Katriel <katriel181920@gmail.com>
2020-09-15 05:24:35 -0500

    codigo limpio

--- ProyectoVisualizacion3D/ProyectoVisualizacion3D.cpp
+++ ProyectoVisualizacion3D/ProyectoVisualizacion3D.cpp
@@ -1,16 +1,7 @@
 #include "ProyectoVisualizacion3D.h"
 
-//#include "OpenGLWidget.h"
-
 ProyectoVisualizacion3D::ProyectoVisualizacion3D(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
-
-    //OpenGLWidget *OGLW = new OpenGLWidget();
-   // ui.verticalLayout->addWidget(OGLW);
-
 }
-//verticalLayout
-//widget
-//openGLWidget
